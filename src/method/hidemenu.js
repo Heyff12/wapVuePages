64eb957225fac66d93c16f661407cadaf1e22c87
chujunfang <chujunfang@qfpay.com>
2018-04-12 19:08:44 +0800

    调整菜单

--- src/method/hidemenu.js
+++ src/method/hidemenu.js
@@ -16,5 +16,26 @@
 import wx from "weixin-js-sdk"
 // wx.hideAllNonBaseMenuItem();
 wx.hideMenuItems({
-    menuList: ["share:appMessage", "share:timeline", "share:qq"] // 要隐藏的菜单项，只能隐藏“传播类”和“保护类”按钮，所有menu项见附录3
+    menuList: [
+            "menuItem:share:appMessage",
+            "menuItem:share:timeline",
+            "menuItem:share:qq",
+            "menuItem:share:weiboApp",
+            "menuItem:favorite",
+            "menuItem:share:facebook",
+            "menuItem:share:QZone",
+            "menuItem:copyUrl",
+            "menuItem:originPage",
+            "menuItem:openWithQQBrowser",
+            "menuItem:openWithSafari",
+            "menuItem:share:email"
+        ] // 要隐藏的菜单项，只能隐藏“传播类”和“保护类”按钮，所有menu项见附录3
+});
+wx.checkJsApi({
+    jsApiList: ["chooseImage"], // 需要检测的JS接口列表，所有JS接口列表见附录2,
+    success: function(res) {
+        // 以键值对的形式返回，可用的api值true，不可用为false
+        // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
+        alert(res)
+    }
 });
